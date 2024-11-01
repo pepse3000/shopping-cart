f6e67640f347ca7b2a7daffd87b4f13d8dd80f58
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-01 23:55:31 +0300

    feat: create main page scheme

---
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -0,0 +1,15 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from "./MainPage.module.scss";
+
+interface MainPageProps {
+    className?: string;
+}
+
+export const MainPage = ({className}: MainPageProps) => {
+
+    return (
+        <div className={classNames(cls.MainPage, {}, [className])}>
+            Main Page
+        </div>
+    )
+}
