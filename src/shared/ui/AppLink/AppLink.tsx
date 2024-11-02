b375ce4da7d457651eb0d1c7d69670f4d1e86446
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-02 22:09:37 +0300

    feat: implement custom link component

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,37 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from "./AppLink.module.scss";
+import {FC, ReactNode} from "react";
+import {Link, LinkProps} from "react-router-dom";
+
+export enum AppLinkThemes {
+    HEADERS = "headers",
+    NAVS = "navs",
+    BACKGROUND = "background"
+}
+
+interface AppLinkProps extends LinkProps {
+    className?: string;
+    theme?: AppLinkThemes;
+    children?: ReactNode;
+    square?: boolean;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+    const {
+        to,
+        className,
+        theme,
+        children,
+        square
+    } = props;
+
+    const appLinkMods = {
+        square,
+    }
+
+    return (
+        <Link to={to} className={classNames(cls.AppLink, appLinkMods, [className, cls[theme]])}>
+            {children}
+        </Link>
+    )
+}
