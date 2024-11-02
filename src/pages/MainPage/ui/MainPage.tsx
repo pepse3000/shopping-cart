de076eb96985996aca60ee7a12fde5415bf8dcec
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-02 17:13:14 +0300

    chore: small scheme fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,27 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./MainPage.module.scss";
+import {PageNavigation} from "widgets/PageNavigation";
+import slide1 from 'shared/assets/slides/slide1.png'
+import {FC} from "react";
 
 interface MainPageProps {
     className?: string;
 }
 
-export const MainPage = ({className}: MainPageProps) => {
+const MainPage: FC = ({className}: MainPageProps) => {
 
     return (
-        <div className={classNames(cls.MainPage, {}, [className])}>
-            Main Page
+        <div className={classNames(cls.MainPage, {}, [className, "page"])}>
+            <div className={cls.navWithSlide}>
+                <PageNavigation />
+                <img src={slide1} alt="Slide 1"/>
+            </div>
+
+            {/*<PageSales />*/}
+            {/*<PageCategories />*/}
+            {/*<PageBestSelling />*/}
         </div>
     )
 }
+
+export default MainPage;
