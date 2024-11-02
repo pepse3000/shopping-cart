cb305ee4bc3575fda63265c0996def236c2cb093
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-02 17:12:09 +0300

    feat: implement header + small page scheme fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,17 @@
 import {Navbar} from "widgets/Navbar/ui/Navbar";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/Router";
+import {Header} from "widgets/Header";
 
 
 function App() {
   return (
       <div className={classNames('app', {}, [])}>
-          <Navbar />
-          <AppRouter />
+          <Header />
+          <div className="page-wrapper">
+              <Navbar />
+              <AppRouter />
+          </div>
       </div>
   )
 }
