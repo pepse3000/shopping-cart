2dd4089f2b5f65ceb261f8b19e3a15b0c6e9de94
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-04 00:00:30 +0300

    chore: add error boundary & store provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { createRoot } from 'react-dom/client'
 import App from 'app/App'
 import 'app/styles/index.scss'
 import {BrowserRouter} from "react-router-dom";
+import {ErrorBoundary} from "app/providers/ErrorBoundary";
+import {StoreProvider} from "app/providers/StoreProvider";
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <App />
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>
 )
