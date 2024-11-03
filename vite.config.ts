e623cbf6d095789294b8177384a15c949a78b382
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-03 23:57:15 +0300

    chore: small code fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import { fileURLToPath, URL } from 'url';
+import svgr from "vite-plugin-svgr";
 import sass from 'sass';
 
 export default defineConfig({
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '@variables', replacement: fileURLToPath(new URL('./src/styles/variables', import.meta.url)) },
+      { find: '@styles', replacement: fileURLToPath(new URL('./src/styles', import.meta.url)) },
     ],
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr({
+    include: "**/*.svg?react",
+      }
+  )],
   css: {
     preprocessorOptions: {
       scss: {
