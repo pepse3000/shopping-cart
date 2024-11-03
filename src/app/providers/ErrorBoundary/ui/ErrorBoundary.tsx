24dc63972126e8950e587462907d56d349c7dee6
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-03 23:55:46 +0300

    feat: create ErrorBoundary comp

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+import React, {ErrorInfo, Suspense} from "react";
+
+interface ErrorBoundaryProps {
+    children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        return error && { hasError: true };
+    }
+
+    componentDidCatch(error: Error, info: ErrorInfo) {
+        console.log(error, info.componentStack);
+    }
+
+    render() {
+        const { hasError } = this.state;
+        const { children } = this.props;
+
+        if (hasError) {
+            return (
+                <Suspense fallback="">
+                    <h1>Errore ...</h1>
+                </Suspense>
+            )
+        }
+
+        return children;
+    }
+}
+
+export default ErrorBoundary;
