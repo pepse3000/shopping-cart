a1734d45d79c7b50aa35f1f7f4f1d9823ad5ebda
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-04 00:00:11 +0300

    feat: implement scroll items with buttons, add product container

--- src/widgets/FlashSales/ui/FlashSales.tsx
+++ src/widgets/FlashSales/ui/FlashSales.tsx
@@ -1,29 +1,57 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./FlashSales.module.scss";
 import {Timer} from "widgets/Timer";
+import {ProductContainer, ProductContainerTypes} from "widgets/ProductContainer/ui/ProductContainer";
+import {Button, ButtonThemes, ButtonTypes} from "shared/ui/Button/Button";
+import {AppLink, AppLinkThemes} from "shared/ui/AppLink/AppLink";
+import {RoutePath} from "shared/config/routeConfig/routeConfig";
+import {useRef} from "react";
+import {Title} from "shared/ui/Title/Title";
 
 interface FlashSalesProps {
     className?: string;
 }
 
 export const FlashSales = ({className}: FlashSalesProps) => {
+    const productContainerRef = useRef<HTMLDivElement>(null);
+
+    const scrollLeft = () => {
+            productContainerRef.current.scrollLeft -= 215;
+    };
+
+    const scrollRight = () => {
+            productContainerRef.current.scrollLeft += 215;
+    };
+
     return (
         <div className={classNames(cls.FlashSales, {}, [className])}>
-            <div className={cls.title}>
-                <div className={cls.box}></div>
-                <p className={cls.head}>Today's</p>
-            </div>
+            <Title name="Today's"/>
             <div className={cls.timer}>
                 <p>Flash Sales</p>
                 <Timer dueTo={1731004591000}/>
                 <div className={cls.btns}>
-                    {/*<Button />*/}
-                    {/*<Button />*/}
+                    <Button
+                        theme={ButtonThemes.ROUND}
+                        types={ButtonTypes.SCROLL_LEFT}
+                        onClick={scrollLeft}
+                    />
+                    <Button
+                        theme={ButtonThemes.ROUND}
+                        types={ButtonTypes.SCROLL_RIGHT}
+                        onClick={scrollRight}
+                    />
                 </div>
             </div>
 
-            {/*<SaleProducts />*/}
-            {/*<Button>View All Products</Button>*/}
+            <ProductContainer ref={productContainerRef} types={ProductContainerTypes.FLASH_SALES}/>
+
+            <div className={cls.centerWrapper}>
+                <AppLink className={cls.link} to={RoutePath.contact} theme={AppLinkThemes.BACKGROUND}>
+                    View All Products
+                </AppLink>
+            </div>
+
+            <div className="line"></div>
         </div>
     )
 }
