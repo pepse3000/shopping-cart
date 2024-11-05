2714cea75373cb8480522b31d93da06b09bf106b
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-05 19:50:47 +0300

    feat: add category request

--- src/entities/Product/model/slices/productService.ts
+++ src/entities/Product/model/slices/productService.ts
@@ -1,18 +1,34 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {IItem} from "entities/Product/model/types/itemInterface";
 
+export enum ProductCategories {
+    CLOTHES = '1143',
+    ELECTRONICS = '1145',
+    FURNITURE = '1146',
+    SHOES = '1144',
+    MISC = '1147'
+}
+
 export const productAPI = createApi({
     reducerPath: 'productAPI',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.escuelajs.co/api/v1'}),
     endpoints: (builder) => ({
-        fetchAllProducts: builder.query<IItem[], number>({
-            query: (limit: number = 10) => ({
+        fetchAllProducts: builder.query<IItem[], { limit: number, offset: number }>({
+            query: ({limit = 10, offset = 0}) => ({
                 url: '/products',
                 params: {
-                    offset: 0,
+                    offset: offset,
                     limit: limit
                 }
             }),
         }),
+        fetchByCategory: builder.query<IItem[], ProductCategories>({
+            query: (category: ProductCategories = ProductCategories.CLOTHES) => ({
+                url: '/products',
+                params: {
+                    categoryId: category
+                }
+            }),
+        })
     })
 })
