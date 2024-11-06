b07c614a2a74665cffa849016ca3b4de28cb43fd
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-11-07 00:07:45 +0300

    chore: create store for cart & wishes, add types

--- src/app/providers/StoreProvider/config/StateInterface.ts
+++ src/app/providers/StoreProvider/config/StateInterface.ts
@@ -1,3 +1,9 @@
-export interface StateInterface {
+import {ICartItem} from "entities/Cart/model/types";
+import {productAPI} from "entities/Product/model/slices/productService";
+import {IWishItem} from "entities/WishList/model/types";
 
+export interface StateInterface {
+    [productAPI.reducerPath]: ReturnType<typeof productAPI.reducer>,
+    cart: ICartItem[],
+    wishes: IWishItem[],
 }
--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,9 +1,13 @@
 import {StateInterface} from "app/providers/StoreProvider/config/StateInterface";
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {productAPI} from "entities/Product/model/slices/productService";
+import cartReducer from 'entities/Cart/model/cartSlice';
+import wishSlice from "entities/WishList/model/wishSlice";
 
 const rootReducer = combineReducers({
-    [productAPI.reducerPath]: productAPI.reducer
+    [productAPI.reducerPath]: productAPI.reducer,
+    cart: cartReducer,
+    wishes: wishSlice,
 })
 
 export function createReduxStore(initialState: StateInterface) {
@@ -15,3 +19,7 @@ export function createReduxStore(initialState: StateInterface) {
         preloadedState: initialState,
     })
 }
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof createReduxStore>
+export type AppDispatch = AppStore['dispatch']
